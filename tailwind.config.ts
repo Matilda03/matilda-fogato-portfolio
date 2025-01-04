2afd369ee819a8b6561e3b225f50d7365bc9d346
Fogato, Matilda <m.fogato@student.tue.nl>
2025-01-04 18:26:24 +0100

    Added customized Tailwind CSS configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import colors from 'tailwindcss/colors'
+import typography from '@tailwindcss/typography'
+
+export default {
+    darkMode: 'class',
+    plugins: [
+        typography(),
+    ],
+    theme: {
+        fontFamily: {
+            sans : ['"Figtree"', 'sans-serif'],
+        },
+        extend: {
+            fontFamily: {
+                inter: ['"Inter"', 'sans-serif'],
+                georgia: ['"Georgia"', 'serif'],
+            }
+        }
+    }
+}
